1d3b37947e77024cfcaa483439223c55295566fc
jbrewer3 <jbrewerbb3@gmail.com>
2021-11-16 21:32:27 -1000

    Updated HiLo

--- hilo.py
+++ hilo.py
@@ -17,6 +17,16 @@ def hilo(answer, low , high):
 
         guesses += 1
 
+correct_count = 0
+max_guesses = 0
+
 for number in range(LOW, HIGH + 1):
     number_of_guesses = hilo(number, LOW, HIGH)
     print("{} guessed in {}".format(number, number_of_guesses))
+
+    if number_of_guesses > max_guesses:
+        max_guesses, correct_count = number_of_guesses, 1
+    elif number_of_guesses == max_guesses:
+        correct_count += 1
+print("I guessed without being tole {} time. Max {} guesses.".
+        format(correct_count, max_guesses))
